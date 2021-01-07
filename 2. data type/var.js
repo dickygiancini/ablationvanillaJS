bbdb307b5f9ec6291584a4a0f559e513e5759d28
DESKTOP-NEUS99T\ASUS <dicky.giancini.123@gmail.com>
2021-01-07 14:58:02 +0700

    data type

---
+++ 2. data type/var.js
@@ -0,0 +1,21 @@
+let myName
+
+myName = "DickyG"
+
+console.log(myName + " is a: " + typeof myName)
+
+const aNumber = 133
+
+console.log(aNumber + " is a: " + typeof aNumber)
+
+let boolType = false
+
+console.log(boolType + " is a: " + typeof boolType)
+
+let arrayType = []
+
+console.log("[] is a: " + typeof arrayType)
+
+const notVar = undefined
+
+console.log("variable undefined is a: " + typeof notVar)
