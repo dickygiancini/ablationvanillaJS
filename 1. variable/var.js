bcdefb31424c3e6fedb70cf4992c4f688bc264bc
DESKTOP-NEUS99T\ASUS <dicky.giancini.123@gmail.com>
2021-01-07 14:45:27 +0700

    variable studies

---
+++ 1. variable/var.js
@@ -0,0 +1,17 @@
+let myName
+
+myName = "DickyG"
+
+console.log(myName)
+
+let lastName
+
+lastName = "Arwindo"
+
+console.log(lastName)
+
+let combineName
+
+combineName = myName + lastName
+
+console.log(combineName)
